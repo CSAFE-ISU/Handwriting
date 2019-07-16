c827a174dace3a66262a8083e713b60f08fba8fb
Amy Crawford <amymc@iastate.edu>
2019-07-16 11:09:21 -0500

    filename

--- DataCollection/app.R
+++ DataCollection/app.R
@@ -293,7 +293,7 @@ server <- function(input, output, session) {
     ### DUMP ORIGINAL INTO ANOTHER FOLDER??
     showNotification(paste0("Procpath: ", paste0(proc_fp, "\\", imageName, ".png")))
     #showNotification("Sample Cropped and Saved!", action = a(href = "javascript:location.reload();", "Reload page"))
-    #file.rename(paste0(stagepath, "\\", input$file1$datapath), paste0(gsub(".png", "", paste0(stagepath, "\\", input$file1$datapath)), "_processed.png"))
+    file.rename(paste0(stagepath, "\\", input$file1$name), paste0(gsub(".png", "", paste0(stagepath, "\\", input$file1$name)), "_processed.png"))
   })
   
   
